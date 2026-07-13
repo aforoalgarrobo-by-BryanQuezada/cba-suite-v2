14bd7aa20305000c587ec8768edd443dcd34dd82
aforoalgarrobo-by-BryanQuezada <aforocuartelescbalgarrobo@gmail.com>
2026-07-12 22:04:33 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyxRpoLEJN3HGIqnv_yZZiz6ZxByhfgZ3CeL6A9PUgWOY3tgbMX23LlPSFgEq9lgfNi/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycbz3d7O8Lvv3YYKKQ4m-6opOjqndEoxwzsGgIzdEcCTapujBrnmviJDfj7VAD3OGcHs_/exec",
   CUARTELES: {
     "1": "1ª Compañía",
-    "2": "2ª Compañía Mirasol",
+    "2": "2ª Compañía",
     "3": "3ª Compañía",
     "general": "Cuartel General"
   },
