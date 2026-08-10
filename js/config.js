946a257e3db4420c0a6eef6a15cae7a93e89a87c
aforoalgarrobo-by-BryanQuezada <aforocuartelescbalgarrobo@gmail.com>
2026-08-10 19:52:55 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,53 @@
 const CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbz3d7O8Lvv3YYKKQ4m-6opOjqndEoxwzsGgIzdEcCTapujBrnmviJDfj7VAD3OGcHs_/exec",
+
+  API_URL:
+    "https://script.google.com/macros/s/AKfycbz3d7O8Lvv3YYKKQ4m-6opOjqndEoxwzsGgIzdEcCTapujBrnmviJDfj7VAD3OGcHs_/exec",
+
   CUARTELES: {
-    "1": "1ª Compañía",
-    "2": "2ª Compañía",
-    "3": "3ª Compañía",
-    "general": "Cuartel General"
+
+    "1":
+      "1ª Compañía",
+
+    "2":
+      "2ª Compañía",
+
+    "3":
+      "3ª Compañía",
+
+    "general":
+      "Cuartel General"
+
   },
-  ESTADOS_BOMBERO: ["Disponible", "No disponible", "En capacitación", "En emergencia"],
-  FOTO_DEFAULT: "img/sinfoto.jpg"
+
+  LOGOS: {
+
+    "1":
+      "img/logo-c1.png",
+
+    "2":
+      "img/logo-c2.png",
+
+    "3":
+      "img/logo-c3.png",
+
+    "general":
+      "img/logo-general.png"
+
+  },
+
+  ESTADOS_BOMBERO: [
+
+    "Disponible",
+
+    "No disponible",
+
+    "En capacitación",
+
+    "En emergencia"
+
+  ],
+
+  FOTO_DEFAULT:
+    "img/sinfoto.jpg"
+
 };
